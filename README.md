e8091bfc609de3168d123f1c963de1a837c183c3
João Pedro Pereira dos Santos <joaop.santos@saude.gov.br>
2026-03-01 13:59:50 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# savestate_manager
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/savestate_manager/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.savestate_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
